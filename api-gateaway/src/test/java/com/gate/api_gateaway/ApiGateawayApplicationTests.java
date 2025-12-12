264e4cda62feec2f3b3e4394d9d86a1fdf28dacc
jad-png <j.belassiria@uca.ca.ma>
2025-12-12 15:12:13 +0100

    config(): add logging config

---
+++ api-gateaway/src/test/java/com/gate/api_gateaway/ApiGateawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gate.api_gateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
